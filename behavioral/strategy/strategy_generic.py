dee6ab8d66fe70edae26c461439a21999ed7032a
vBarbaros <victor.barbarosh@gmail.com>
2022-08-08 21:51:17 -0400

    Implement Interface and subclasses for  strategy pattern (2)

--- behavioral/strategy/strategy_generic.py
+++ behavioral/strategy/strategy_generic.py
@@ -1,6 +1,41 @@
-# source : https://www.tutorialspoint.com/python_design_patterns/python_design_patterns_strategy.htm
-# with small adaptations to keep same style across examples
+# sources :
+# https://www.tutorialspoint.com/python_design_patterns/python_design_patterns_strategy.htm
+# https://realpython.com/python-interface/ (example of interface implementation in Python)
+
+import abc
+
+
+class StrategyInterface(metaclass=abc.ABCMeta):
+    @classmethod
+    def __subclasshook__(self, subclass):
+        return (hasattr(subclass, 'algorithm_interface') and
+                callable(subclass.algorithm_interface))
+
+    @abc.abstractmethod
+    def algorithm_strategy(self, attribute: str) -> str:
+        raise NotImplementedError
+
+
+class ConcreteStrategyA():
+    def algorithm_interface(self, attribute: str) -> str:
+        """Overrides StrategyInterface.algorithm_interface()"""
+        pass
+
+
+class ConcreteStrategyB():
+    def algorithm_interface(self, attribute: str) -> str:
+        """Overrides StrategyInterface.algorithm_interface()"""
+        pass
+
+
+class ConcreteStrategyC():
+    def algorithm_interface(self, attribute: str) -> str:
+        """Overrides StrategyInterface.algorithm_interface()"""
+        pass
 
 
 if __name__ == "__main__":
-    pass
+    print(issubclass(ConcreteStrategyA, StrategyInterface))
+    print(issubclass(ConcreteStrategyB, StrategyInterface))
+    print(issubclass(ConcreteStrategyC, StrategyInterface))
+    print(ConcreteStrategyA.__mro__)
