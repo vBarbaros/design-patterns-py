5dbf47f5d4297446f463ec08e819e3f53df1bc98
vBarbaros <victor.barbarosh@gmail.com>
2022-10-02 20:08:30 -0400

    WIP: Builder pattern

--- creational/builder/builder_generic.py
+++ creational/builder/builder_generic.py
@@ -1,6 +1,38 @@
-# source : https://www.tutorialspoint.com/python_design_patterns/python_design_patterns_strategy.htm
-# with small adaptations to keep same style across examples
+class Builder:
+    def build_part(self): pass
+
+
+class ConcreteBuilder(Builder):
+    def build_part(self):
+        print('build part A')
+        print('build part B')
+        print('build part C')
+        return self.get_result()
+
+    def get_result(self):
+        return 'Final Built (part A, part B, Part C)'
+
+
+class Director():
+    def __init__(self, builder):
+        self.builder = builder
+        self.result = ''
+
+    def construct(self):
+        self.result = self.builder.build_part()
+
+    def show_result(self):
+        print(self.result)
+
+
+def main_object_builder_pattern():
+    print('...creating Director...')
+    adapter = Director(ConcreteBuilder())
+    print('...calling constructor()...')
+    adapter.construct()
+    print('...displaying the built result...')
+    adapter.show_result()
 
 
 if __name__ == "__main__":
-    pass
+    main_object_builder_pattern()
